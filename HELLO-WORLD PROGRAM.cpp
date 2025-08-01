6ac75cd07ad16a6aa7b691c928bb510ed55617c9
vaishnavipangare <vaishnavi.pangare.btech2024@sitpune.edu.in>
2025-08-01 06:34:04 -1200

    Create HELLO-WORLD PROGRAM.cpp

---
+++ HELLO-WORLD PROGRAM.cpp
@@ -0,0 +1,21 @@
+//EXPERIMENT - 1
+//VAISHNAVI PANGARE
+//ENTC-A3
+//PRN-25070123501  
+///Problem Statement: Write a simple HELLO WORLD program using C++.
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout <<"HELLO WORLD\n" ;
+  cout <<"HI!I AM PALAK SONI";
+   
+  return 0;
+}
+
+/*Output- 
+  HELLO WORLD
+  HI!I AM PALAK SONI
+  */
